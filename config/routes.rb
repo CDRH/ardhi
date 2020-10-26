cb2e65b0b7de0c5f5f724ce6d833435b161abc97
Karin Dalziel <kdalziel2@unl.edu>
2020-10-26 16:07:53 -0500

    deleted code

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/map', to: 'items#map', as: 'map'
+  get '/timeline', to: 'general#timeline', as: 'timeline'
 
 end
