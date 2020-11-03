1a6f5b4521350c81078e2188845a0900f4af8c01
Jessica Dussault <jdussault4@gmail.com>
2020-11-03 09:46:05 -0600

    highlights map nav

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,3 +1,23 @@
 module ItemsHelper
   include Orchid::ItemsHelper
+
+  def map_selected_title
+    if params["f"].present?
+      f = params["f"]
+      title = params["f"].select { |p| p.include?("title|") }.first
+      if title
+        title.split("|").last
+      end
+    end
+  end
+
+  def map_nav_class(nav_title, selected_title)
+    if nav_title == selected_title ||
+        (!selected_title && nav_title.nil?)
+      "btn btn-primary"
+    else
+      "btn btn-default"
+    end
+  end
+
 end
